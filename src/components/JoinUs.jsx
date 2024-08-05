eeef8741afc583e47bad71c30bb3f35494f10586
7859950358 <mayurbhalu135@gmail.com>
2024-08-05 18:33:42 +0530

    add file

---
+++ src/components/JoinUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function JoinUs() {
+    return (
+        <>
+            <section class="section2">
+                <div class="container tw-pt-16">
+                    <div class="col-12 col-md-9 col-lg-8 col-xl-8 col-xxl-7">
+                        <h3 class="fs-5 mb-3 text-white text-uppercase"><mark
+                            class="text-white bsb-tpl-highlight bg-text2 tw-text-white text-text">UNLOCK FRESH PROESPECTS</mark></h3>
+                        <h2 class="display-4 text-white fw-bolder mb-4 pe-xl-5">We are a design agency studio delivering custom
+                            creative &amp; unique websites.</h2>
+                        <button
+                            class="tw-border-solid  tw-rounded hover:tw-bg-white hover:tw-text-red-800 hover:tw-border-red-800 hover:tw-border-solid tw-duration-300 tw-py-2 tw-p-5 join-us">Join
+                            Us</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default JoinUs
